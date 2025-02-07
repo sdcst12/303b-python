d335cd1d144438bf7d3012b7297b432935771970
hungrybeagle <cyang@deltalearns.ca>
2025-02-07 07:54:32 -0800

    Initial commit

---
+++ assignment.py
@@ -0,0 +1,24 @@
+#!python
+
+"""
+Create a program that will store the database for a veterinary
+Each record needs to have the following information:
+id unique integer identifier
+pet name
+pet species (cat, bird, dog, etc)
+pet breed (persian, beagle, canary, etc)
+owner name
+owner phone number
+owner email
+owner balance (amount owing)
+date of first visit
+
+create a program that will allow the user to:
+insert a new record into the database and save it automatically
+retrieve a record by their id and display all of the information
+retrieve a record by the email and display all of the information
+retrieve a record by phone number and display all of the information
+
+You will need to create the table yourself. Consider what data types you will
+need to use.
+"""
